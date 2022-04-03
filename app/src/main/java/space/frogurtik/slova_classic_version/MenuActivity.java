5dab218a49554ead13abf8bad6ed029a795cbae0
Dora Res <90782888+frogurtik@users.noreply.github.com>
2022-04-03 22:56:55 +0800

    commit
    SLOVA

---
+++ app/src/main/java/space/frogurtik/slova_classic_version/MenuActivity.java
@@ -0,0 +1,74 @@
+package space.frogurtik.slova_classic_version;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class MenuActivity extends AppCompatActivity {
+    EditText ET_count_of_gamers, ET_names_of_gamers;
+    Button BTN_to_start_game, BTN_to_rules;
+    Dialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ET_count_of_gamers = findViewById(R.id.count_of_players);
+        ET_names_of_gamers = findViewById(R.id.names_of_players);
+        BTN_to_start_game = findViewById(R.id.btn_to_start_game);
+        BTN_to_rules = findViewById(R.id.btn_to_rules);
+
+        BTN_to_start_game.setOnClickListener(view -> game_classic());
+        BTN_to_rules.setOnClickListener(view -> to_rules());
+
+    }
+
+    void game_classic(){
+        String count_of_gamers = ET_count_of_gamers.getText().toString();
+        String s = ET_names_of_gamers.getText().toString();
+
+        if (TextUtils.isEmpty(count_of_gamers) || TextUtils.isEmpty(s)){
+            Toast.makeText(getApplicationContext(), "Не все необходиые данные заполнены", Toast.LENGTH_SHORT).show();
+        }else{
+                TreeSet<String> names_of_gamers = new TreeSet<>();
+                String[] arr = s.split(" ");
+                for (int i = 0; i < arr.length; i++) {
+                    names_of_gamers.add(arr[i]);
+                }
+                if (names_of_gamers.size() != Integer.parseInt(count_of_gamers)){
+                    Toast.makeText(getApplicationContext(), "Данные не сходятся", Toast.LENGTH_SHORT).show();
+                }else{
+                    int i = Integer.parseInt(count_of_gamers);
+                    if (i == 1){
+                        Toast.makeText(getApplicationContext(), "К сожалению, нельзя играть ни с кем :( ", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+                        intent.putExtra("count_people", count_of_gamers);
+                        intent.putExtra("spisok_names", arr);
+                        startActivity(intent);
+                    }
+                }
+            }
+        }
+
+    void to_rules(){
+        dialog = new Dialog(MenuActivity.this);
+        dialog.setTitle("Заголовок диалога");
+        dialog.setContentView(R.layout.dialog_view);
+        dialog.show();
+    }
+    }
